b0b7aa622dfc2cea3097ee1a514697b2a0f855d4
yangGuang <yangguang@MacBook-Pro-3.local>
2022-10-31 16:54:49 +0800

    Initial commit

---
+++ src/main/java/com/xfc/project_xfc/ProjectXfcApplication.java
@@ -0,0 +1,13 @@
+package com.xfc.project_xfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectXfcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xfc/project_xfc/ProjectXfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfc.project_xfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectXfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
